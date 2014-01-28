c9bc3fb9c40869d1ccf973d873adba640c272269
Daniel Burke <danieljburke@me.com>
2014-01-28 22:03:48 +1100

    Demonstrate button background colors.

--- PXAlertViewDemo/PXViewController.m
+++ PXAlertViewDemo/PXViewController.m
@@ -68,6 +68,7 @@
     [alert setCancelButtonBackgroundColor:[UIColor redColor]];
     [alert setTitleFont:[UIFont fontWithName:@"Zapfino" size:15.0f]];
     [alert setTitleColor:[UIColor darkGrayColor]];
+    [alert setCancelButtonBackgroundColor:[UIColor redColor]];
 }
 
 
