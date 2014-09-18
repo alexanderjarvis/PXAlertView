b01f16b1dc8aa6914318bd621ed69d48cdb1c469
Bryan Way <bryanwayb@gmail.com>
2014-09-18 14:30:29 -0500

    Swizzling optional

--- Classes/UIAlertView+PXAlertViewOverride.h
+++ Classes/UIAlertView+PXAlertViewOverride.h
@@ -5,6 +5,11 @@
 //  Created by Bryan Way on 18/09/2014.
 //
 
+// Uncomment next line to enable automatic implementation
+//#define PXALERT_SWIZZLING
+
+#ifdef PXALERT_SWIZZLING
+
 #import "PXAlertView.h"
 #import <objc/runtime.h>
 
@@ -13,3 +18,5 @@
 -(void)showWithContent:(UIView*)contentView;
 
 @end
+
+#endif
